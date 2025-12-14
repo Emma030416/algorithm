3ffeafe3d87aded4a274a261c009442afc3575bd
Emma030416 <1658239747@qq.com>
2025-12-14 23:13:58 +0800

    Add files via upload

---
+++ 第二期/贪心算法demo.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <map>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <numeric>
+#include <functional>
+#include <ranges>
+#include <iomanip>
+#include <chrono>
+//#define int long long //赫赫 要不要龙龙呢
+using namespace std;
+
+signed main()
+{
+    auto T_start=chrono::steady_clock::now();
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    //ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    
+    return 0;
+}
